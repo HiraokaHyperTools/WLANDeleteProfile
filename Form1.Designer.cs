149b4c81f8904c35722dc1737a724acbe3797308
kenjiuno <h8mastre@gmail.com>
2016-06-20 17:37:20 +0900

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WLANDeleteProfile {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lvW = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chSSID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAuth = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cmsW = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mRemove = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsW.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvW
+            // 
+            this.lvW.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chSSID,
+            this.chAuth});
+            this.lvW.ContextMenuStrip = this.cmsW;
+            this.lvW.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvW.FullRowSelect = true;
+            this.lvW.GridLines = true;
+            this.lvW.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvW.Location = new System.Drawing.Point(0, 0);
+            this.lvW.Name = "lvW";
+            this.lvW.Size = new System.Drawing.Size(716, 469);
+            this.lvW.TabIndex = 0;
+            this.lvW.UseCompatibleStateImageBehavior = false;
+            this.lvW.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "名称";
+            this.chName.Width = 250;
+            // 
+            // chSSID
+            // 
+            this.chSSID.Text = "SSID";
+            this.chSSID.Width = 250;
+            // 
+            // chAuth
+            // 
+            this.chAuth.Text = "認証";
+            this.chAuth.Width = 150;
+            // 
+            // cmsW
+            // 
+            this.cmsW.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mRemove});
+            this.cmsW.Name = "cmsW";
+            this.cmsW.Size = new System.Drawing.Size(118, 26);
+            // 
+            // mRemove
+            // 
+            this.mRemove.Name = "mRemove";
+            this.mRemove.Size = new System.Drawing.Size(117, 22);
+            this.mRemove.Text = "削除する";
+            this.mRemove.Click += new System.EventHandler(this.mRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 469);
+            this.Controls.Add(this.lvW);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WLANDeleteProfile";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cmsW.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvW;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chSSID;
+        private System.Windows.Forms.ColumnHeader chAuth;
+        private System.Windows.Forms.ContextMenuStrip cmsW;
+        private System.Windows.Forms.ToolStripMenuItem mRemove;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WLANDeleteProfile {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Upd();
+        }
+
+        private void Upd() {
+            lvW.Enabled = false; Update();
+            try {
+                String dir = Path.Combine(Path.GetTempPath(), "WLANDeleteProfile_" + DateTime.Now.Ticks);
+                Directory.CreateDirectory(dir);
+                var psi = new ProcessStartInfo("netsh.exe", " wlan export profile ");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                psi.WorkingDirectory = dir;
+                var p = Process.Start(psi);
+                p.WaitForExit();
+                lvW.Items.Clear();
+                foreach (String fp in Directory.GetFiles(dir, "*.xml")) {
+                    XmlDocument xmlo = new XmlDocument();
+                    xmlo.Load(fp);
+                    var xnm = new XmlNamespaceManager(xmlo.NameTable);
+                    xnm.AddNamespace("a", "http://www.microsoft.com/networking/WLAN/profile/v1");
+                    var Name = (xmlo.SelectSingleNode("/a:WLANProfile/a:name/text()", xnm) ?? xmlo.CreateTextNode("")).Value;
+                    var SSID = (xmlo.SelectSingleNode("/a:WLANProfile/a:SSIDConfig/a:SSID/a:name/text()", xnm) ?? xmlo.CreateTextNode("")).Value;
+                    var Auth = (xmlo.SelectSingleNode("/a:WLANProfile/a:MSM/a:security/a:authEncryption/a:authentication/text()", xnm) ?? xmlo.CreateTextNode("")).Value;
+
+                    var lvi = new ListViewItem(Name);
+                    lvi.SubItems.Add(SSID);
+                    lvi.SubItems.Add(Auth);
+                    lvW.Items.Add(lvi);
+                }
+                Directory.Delete(dir, true);
+            }
+            finally {
+                lvW.Enabled = true;
+            }
+        }
+
+        private void mRemove_Click(object sender, EventArgs e) {
+            if (MessageBox.Show(this, "本当に、削除しますか。", Text, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != System.Windows.Forms.DialogResult.Yes)
+                return;
+
+            bool any = false;
+            foreach (var lvi in lvW.SelectedItems.Cast<ListViewItem>()) {
+                var psi = new ProcessStartInfo("netsh.exe", " wlan delete profile name=\"" + lvi.Text + "\" ");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                var p = Process.Start(psi);
+                p.WaitForExit();
+                any |= p.ExitCode == 0;
+            }
+
+            if (any) Upd();
+        }
+    }
+}
